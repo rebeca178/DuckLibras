64214630c77f8a54645a8c14712ec2a69bb97dcd
Jand1lson <manuela.j.gamer1968@gmail.com>
2025-02-21 16:55:10 -0300

    Tabela usuario/aulas concluída

--- db.sql
+++ db.sql
@@ -3,3 +3,30 @@
 --FlashCards:Guilherme
 --Anotacoes:Ana Clara
 --Aulas:Manu 
+
+create database Aulas;
+
+use Aulas;
+
+
+create table usuario(
+id int(10) unique not null,
+sinal varchar(20),
+explicaçao text);
+
+
+
+create table videos(
+
+id int(4) not null,
+atividades  text(500),
+resumo text(500),
+nivel_id int(5) not null
+
+
+);
+
+
+create table 
+
+
