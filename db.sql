088204e762b710b92cce6646dc27f677b88a51a9
roodguees <rebecafreitasreis7@gmail.com>
2025-02-21 16:44:36 -0300

    tabela completa

--- db.sql
+++ db.sql
@@ -1,5 +1,13 @@
 --aluno:Dhermeson 
 --boardSquare(BS):rebeca
+CREATE DATABASE BS;
+USE BS;
+
+Create table BS(
+    id INT(10) NOT NULL AUTO INCREMENT,
+    STATUS BOOLEAN NOT NULL,
+
+);
 --FlashCards:Guilherme
 --Anotacoes:Ana Clara
 --Aulas:Manu 
