b68c8915ea217cd7d96aa56395e6a5ff98746c65
unknown <dhemerson.juniorr6000@gmail.com>
2025-02-21 16:21:10 -0300

    Tabela usuaria Concluida

--- db.sql
+++ db.sql
@@ -1,4 +1,13 @@
 --aluno
+CREATE DATABASE aluno;
+USE aluno;
+
+Create table aluno(
+    id INT(10) NOT NULL AUTO INCREMENT,
+    nome VARCHAR(55) NOT NULL,
+
+);
+
 --boardSquare
 --FlashCards
 --Anotacoes 
