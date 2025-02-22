069ae5d63eb72dbcb3c079b98149552a023cde4d
Robson Elias <elias.freitas1205@gmail.com>
2025-02-22 00:22:44 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -14,3 +14,8 @@ CREATE TABLE SuaTabela (
 --created_at e updated_at: Adicionamos colunas para registrar automaticamente a data e hora de criação e atualização do registro, ajudando a rastrear alterações no banco de dados.
 --ENGINE=InnoDB: Utilizamos o mecanismo InnoDB, que oferece suporte a transações e integridade referencial.
 -- <3
+
+--boardSquare
+--FlashCards
+--Anotacoes 
+--Aulas 
