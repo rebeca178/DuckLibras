ea6d7f4e457e1f9a9758350de8cfb76157ac5f43
João Augusto Forne Pereira <fornepereiraj@gmail.com>
2025-02-23 13:50:16 -0400

    tabela de tradução concluída

--- db.sql
+++ db.sql
@@ -3,3 +3,15 @@
 --FlashCards:Guilherme
 --Anotacoes:Ana Clara
 --Aulas:Manu 
+
+--Tradução
+CREATE TABLE Traducao_Libras (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    palavra VARCHAR(255) NOT NULL, 
+    sinal VARCHAR(255) NOT NULL, 
+    imagem VARCHAR(255) NULL, 
+    explicacao TEXT NOT NULL, 
+    status BOOLEAN NOT NULL DEFAULT FALSE, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
