b019d6f04dc3b2c7d034993e259ca1588984a466
Jand1lson <manuela.j.gamer1968@gmail.com>
2025-02-24 16:43:28 -0300

    Tabela usuario/aulas concluída

--- db.sql
+++ db.sql
@@ -10,3 +10,12 @@ resumo text(500),
 nivel_id int(5) not null,
 resultado number ,
 );
+
+
+
+--id_usuario é a identificação do usuário.
+--id_videos é a identificação dos vídeos nas aulas.
+--id_nivel é a identificação dos squareboard e seus níveis.
+--explicação é o texto que vai ter em cada atividade.
+--resumo vai ter em todas em squaresboard e dicas.
+--resultado é das atividades e provas para passar para o próximo nível.
