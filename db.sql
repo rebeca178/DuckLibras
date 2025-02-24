191361397ec306aff09dd4453e89162cd0465fb7
Robson Elias <elias.freitas1205@gmail.com>
2025-02-24 17:57:37 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,18 +1,11 @@
+--Tabela Anotações 
 CREATE TABLE Anotacoes (
-    id INT PRIMARY KEY AUTO_INCREMENT, -- Identificador único gerado automaticamente
-    titulo VARCHAR(255) NOT NULL, -- Título da anotação
-    conteudo TEXT NOT NULL, -- Conteúdo da anotação
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data e hora da criação da anotação
-    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Data e hora da última atualização
-) ENGINE=InnoDB;
-
--- melhorias e afins :D
--- AUTO_INCREMENT: O id será gerado automaticamente pelo banco de dados, facilitando a inserção de novos registros.
--- NOT NULL: Garantimos que titulo e conteudo não possam ser nulos, exigindo que sempre sejam fornecidos valores.
--- TEXT: O tipo TEXT permite armazenar conteúdos mais longos para as anotações.
--- data_criacao e data_atualizacao: Adicionamos colunas para registrar automaticamente a data e hora de criação e atualização, ajudando a rastrear alterações no banco de dados.
--- ENGINE=InnoDB: Utilizamos o mecanismo InnoDB, que oferece suporte a transações e integridade referencial.
--- <3
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    conteudo TEXT NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 --boardSquare
 --FlashCards
