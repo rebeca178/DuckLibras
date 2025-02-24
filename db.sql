1bc1321a9e9cf771d339939c7d3849c2e9c3435d
pinguimemo <rebecafreitasreis7@gmail.com>
2025-02-24 15:56:33 -0300

    db.sql

--- db.sql
+++ db.sql
@@ -1,13 +1,4 @@
 
-
--- melhorias e afins :D
---AUTO_INCREMENT: O id será gerado automaticamente pelo banco de dados, facilitando a inserção de novos registros.
---NOT NULL: Garantimos que sinal, explicacao e status não possam ser nulos, exigindo que sempre sejam fornecidos valores.
---VARCHAR(500): Aumentamos o tamanho máximo da coluna explicacao para 500 caracteres, permitindo descrições mais longas.
---created_at e updated_at: Adicionamos colunas para registrar automaticamente a data e hora de criação e atualização do registro, ajudando a rastrear alterações no banco de dados.
---ENGINE=InnoDB: Utilizamos o mecanismo InnoDB, que oferece suporte a transações e integridade referencial.
--- <3
-
 --boardSquare
 --FlashCards
 --Anotacoes 
