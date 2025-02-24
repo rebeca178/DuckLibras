9a13f7c43b045f5a960b8e33c08506b48c5b072e
pinguimemo <rebecafreitasreis7@gmail.com>
2025-02-24 15:55:55 -0300

    db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,4 @@
-CREATE TABLE SuaTabela (
-    id INT PRIMARY KEY AUTO_INCREMENT,-- Identificador único gerado automaticamente 
-    sinal TINYINT NOT NULL,-- Valor do sinal (de -128 a 127)
-    explicacao VARCHAR(500) NOT NULL,
-    status BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,-- Data e hora da criação do registro
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- Data e hora da última atualização
-) ENGINE=InnoDB;
+
 
 -- melhorias e afins :D
 --AUTO_INCREMENT: O id será gerado automaticamente pelo banco de dados, facilitando a inserção de novos registros.
