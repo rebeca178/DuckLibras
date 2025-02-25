674c5bee1299e924e4f15be283899c7c343a9316
ManubsProgramming <manuela.j.gamer1968@gmail.com>
2025-02-25 17:42:55 -0300

    Update db.sql1.0

--- db.sql
+++ db.sql
@@ -22,6 +22,8 @@ FOREIGN KEY (id_anotaccao) REFERENCES ANOTACOES(id_anotaccao)
 
 
 
+
+
 --boardSquare
 --FlashCards
 --Anotacoes 
@@ -35,19 +37,83 @@ CREATE TABLE FLASHCARD(
      sinais string (255)
      
 );
-
-
+-----TABELA AULA-------------------------
 create table aulas
 (
-id_aulas int(10) PRIMARY KEY unique not null,
-sinal varchar(20),
+id_aula primary key ,
+id_usuario int(10) unique not null,
+idsinal  primary key varchar(20),
 explicaçao text(255),
 id_videos int(4) not null,
 atividades  text(500),
 resumo text(500),
 nivel_id int(5) not null,
-resultado number 
+resultado int(10)
 );
+ --idsinal identificação dos sinais passados.
+--id_aula é a identificação das aulas.
+--id_usuario é a identificação do usuário.
+--id_videos é a identificação dos vídeos nas aulas.
+--id_nivel é a identificação dos squareboard e seus níveis.
+--explicação é o texto que vai ter em cada atividade.
+--resumo vai ter em todas em squaresboard e dicas.
+--resultado é das atividades e provas para passar para o próximo nível.
+-
+
+create table videos(
+
+id_videos int(4) not null,
+videos mp4 ,
+descricao text(255),
+categoria_video text(32),
+id_categoria int not null
+);
+
+--id_videos é a identificação dos vídeos nas aulas.
+--videos serão passados as respectivas aulas.
+--descricao dos videos para leitura.
+--categoria_video ex:categoria cumprimento sinal: olá!
+--id_categoria identificação das categorias.
+
+create table sinal(
+
+   idsinal primary key not null,
+   nome varchar(30) not null,
+   video mp4 ,
+   descricao text(255),
+   id_categoria primary key int not null
+);
+
+
+--idsinal identificação dos sinais passados.
+--nome dos sinais.
+--video onde será passado os sinais.
+--descricao dos sinais.
+--id_categoria identificação da categoria.
+
+create table categorias(
+    id_categoria primary key,
+    nome varchar(255),
+    categoria_video  text(255)
+
+);
+
+--id_categoria identificação da categoria.
+--nome da categoria.
+--categoria_video ex:categoria cumprimento sinal: olá!
+
+create table perguntas(
+    id_pergunta primary key,
+    id_quiz primary key,
+    pergunta varchar(255),
+    respostacorreta varchar(20)
+);
+
+--id_pergunta identificação da pergunta.
+--id_quiz identificação do quiz.
+--pergunta para as atividades das aulas.
+--respostacorreta onde terá o gabarito do quiz.
+
 
 --Tabela Anotações 
 CREATE TABLE Anotacoes (
