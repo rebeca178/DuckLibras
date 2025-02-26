989da8f7b1de20720fbb96c45be4da22343f9bd6
Robson <elias.freitas1205@gmail.com>
2025-02-26 15:44:37 -0300

    Adiciona dados ficticios na tabela de anotacoes

---
+++ pupulateAnotacoes.sql
@@ -0,0 +1,8 @@
+-- population.sql
+
+INSERT INTO Anotacoes (titulo, conteudo, data_criacao, data_atualizacao, sinais, explicacao) VALUES
+('Título 1', 'Conteúdo da anotação 1', NOW(), NOW(), 'Sinais exemplo 1', 'Explicação do sinal 1'),
+('Título 2', 'Conteúdo da anotação 2', NOW(), NOW(), 'Sinais exemplo 2', 'Explicação do sinal 2'),
+('Título 3', 'Conteúdo da anotação 3', NOW(), NOW(), 'Sinais exemplo 3', 'Explicação do sinal 3'),
+('Título 4', 'Conteúdo da anotação 4', NOW(), NOW(), 'Sinais exemplo 4', 'Explicação do sinal 4'),
+('Título 5', 'Conteúdo da anotação 5', NOW(), NOW(), 'Sinais exemplo 5', 'Explicação do sinal 5');
