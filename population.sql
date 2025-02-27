79f4685c37a33657d334a6a091021a86c931a046
Robson Elias <elias.freitas1205@gmail.com>
2025-02-27 11:06:39 -0400

    Update population.sql

--- population.sql
+++ population.sql
@@ -1,1 +1,4 @@
+use DuckLibras;
 
+INSERT INTO Anotacoes (titulo, conteudo, sinais, explicacao) 
+VALUES ('Importância da Libras', 'A Libras é essencial para a comunicação.', 'sinal1', 'Esta anotação explica como a Libras facilita a inclusão.');
