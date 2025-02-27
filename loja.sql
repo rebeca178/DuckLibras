0744a9e105675962a28c25c811ca58148eadbd91
unknown <fornepereiraj@gmail.com>
2025-02-27 14:56:29 -0300

    loja atualizada

--- loja.sql
+++ loja.sql
@@ -4,6 +4,7 @@ CREATE TABLE loja (
     id_product INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(100) NOT NULL,
     descricao TEXT,
+    imagem VARCHAR (255) NOT NULL,
     preco DECIMAL(10, 2) NOT NULL,
     data_lancamento DATE
 );
