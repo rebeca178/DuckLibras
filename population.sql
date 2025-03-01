d94b3d5915e9f08385075cc6e0292a3d6a8b20a4
pinguimemo <rebecafreitasreis7@gmail.com>
2025-03-01 17:35:19 -0300

    population.sql

--- population.sql
+++ population.sql
@@ -1,6 +1,3 @@
 use DuckLibras;
 
-insert aulas (id_aula,id_usuario,idsinal,explicaçao,id_videos ,atividades,resumo ,nivel_id,resultado ,id_pergunta,id_quiz,pergunta,respostacorreta) VALUES  ('1','1','2','pipipopo','1','popopipi','huahua','1','1','1','1','what','this');
-insert into FLASHCARD (id_flashcard,ponto,quest,sinais) VALUES  ('1','1','arroz','feijao');
-
 insert into BS (id_bs,STATUS,id_aula,id_flashcard) VALUES  ('1','1','1','1');
