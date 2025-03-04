2dfead2c6722f5477805d0319979d3df58802856
Prestigiado <74106934+Prestigiado@users.noreply.github.com>
2025-03-04 04:51:24 -0300

    db.sql

--- db.sql
+++ db.sql
@@ -1,17 +1,17 @@
 create table chatbot(
-    chat int primary key auto_increment,
+    id_chat int primary key auto_increment,
     perguntas varchar(255),
     respostas varchar(255)
 );
 
 create table loja(
-    lol int primary key auto_increment,
+    id_lol int primary key auto_increment,
     pato varchar(255),
     preco varchar(255)
 );
 
 create table compra(
-    cop int primary key auto_increment,
+    id_cop int primary key auto_increment,
     quantidade int,
     foreign key (loja_id) references loja(lol)
 ); 
