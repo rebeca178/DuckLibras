565ed5f3b764edc8a30e81100cf3ef7a19182ec7
Desktop <dhemerson.juniorr6000@gmail.com>
2025-03-04 04:54:15 -0300

    Feito com Sucesso Tabela Aluno

--- db.sql
+++ db.sql
@@ -1,60 +1,3 @@
-create database DuckLibras
-DEFAULT CHARACTER SET utf8mb4
-DEFAULT COLLATE utf8mb4_general_ci;
-use DuckLibras;
-
-CREATE TABLE FLASHCARD(
-     id_flashcard INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-     ponto INT,
-     quest VARCHAR(255) NOT NULL,
-     sinais VARCHAR(255) 
-     
-);
-
-CREATE TABLE Anotacoes (
-    id_anotacao INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(255) NOT NULL,
-    conteudo TEXT NOT NULL,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    sinais VARCHAR(255) ,
-    imagens VARCHAR(255) ,
-    explicacao TEXT NOT NULL
-);
-
-CREATE TABLE Traducao_Libras (
-    id_tradu INT PRIMARY KEY AUTO_INCREMENT, 
-    palavra VARCHAR(255) NOT NULL, 
-    sinal VARCHAR(255) NOT NULL, 
-    imagem BLOB NULL, 
-    explicacao TEXT NOT NULL, 
-    status BOOLEAN NOT NULL DEFAULT FALSE, 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-create table aulas
-(
-id_aulas int(10) PRIMARY KEY unique not null AUTO_INCREMENT,
-sinal varchar(20),
-explicaçao text(255),
-videos text(255),
-nivel  int(5) not null,
-status BOOLEAN
-);
-
-
-Create table BS(
-
-    id_bs INT(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    STATUS BOOLEAN NOT NULL,
-    nivel  int(5) not null,
-    id_aula int, 
-    id_flashcard int,
-    FOREIGN KEY (id_aula) REFERENCES aulas(id_aula),
-    FOREIGN KEY (id_flashcard) REFERENCES FLASHCARD(id_flashcard)
-);
-
 Create table aluno(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     username VARCHAR(100) NOT NULL,
@@ -63,11 +6,18 @@ Create table aluno(
     senha VARCHAR(255) NOT NULL,
     nivel int,
     ponto int,
-	id_bs INT,
-    id_flashcard INT,
-    id_anotacao INT,
+	id_bs int,
+    id_flashcard int,
+    id_anotacao int,
+    id_chat int,
+    id_loja int,
+    id_cop int,
 
 FOREIGN KEY (id_bs) REFERENCES BS(id_bs),
 FOREIGN KEY (id_flashcard) REFERENCES FLASHCARD(id_flashcard),
-FOREIGN KEY (id_anotacao) REFERENCES Anotacoes(id_anotacao)
+FOREIGN KEY (id_anotacao) REFERENCES Anotacoes(id_anotacao),
+FOREIGN KEY (id_chat) REFERENCES ChatBOT(id_chat),
+FOREIGN KEY (id_loja) REFERENCES Loja(id_loja),
+FOREIGN KEY (id_cop) REFERENCES compras(id_cop),
+
 );
