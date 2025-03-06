572fd991fa7d89ed81824b5e6fae2d94c5a7c18a
pinguimemo <rebecafreitasreis7@gmail.com>
2025-03-06 15:56:23 -0300

    population.sql

--- population.sql
+++ population.sql
@@ -1,3 +1,8 @@
 use DuckLibras;
 
-insert into BS (id_bs,STATUS,id_aula,id_flashcard) VALUES  ('1','1','1','1');
+insert into BS (id_bs,STATUS,id_aula,id_flashcard,nivel) VALUES  ('1','1','1','1','1');
+
+FLASH_AL(id_aluno,id_flashcard) VALUES  ('1','1');
+
+AU_AL(id_aula,id_aluno) VALUES  ('1','1');
+
