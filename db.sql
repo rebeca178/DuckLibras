c403e1c6f88c1922ba8142d7cd1482bec6ba4ada
ManubsProgramming <manuela.j.gamer1968@gmail.com>
2025-03-06 17:00:33 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,14 +1,16 @@
+create database DuckLibras
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+use DuckLibras;
 
 
-create table aulas
-(
+create table aulas (
 id_aulas int(10) PRIMARY KEY unique not null AUTO_INCREMENT,
-sinal varchar(20),
+sinal varchar(500),
 explicaçao text(255),
 videos text(255),
-nivel  int(5) not null,
+nivel  int(255) not null,
 status BOOLEAN
+FOREIGN KEY (id_traducao) REFERENCES traduçao(id_traducao) ON DELETE SET NULL,
 );
-
-
 
