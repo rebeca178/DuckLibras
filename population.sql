7a6a4b7b36c74dd530d302ee5597bea2e129436f
pinguimemo <rebecafreitasreis7@gmail.com>
2025-03-07 16:16:10 -0300

    population.sql

--- population.sql
+++ population.sql
@@ -1,2 +1,4 @@
 use DuckLibras;
-insert into FLASHCARD (quest, SINAL, Img, Lid ) values ('Cachorro', 'www.libraslab.com.br', 'www.libraslab.com.br', 1);
+insert into FLASHCARD (ID_FLASHCARD,quest,Pid,DICIOid,DICASid ) values ('1', 'WHAT?', '1','1','1');
+
+insert into Dicas (ID_dicas,ajuda,Fid,Cid,DICIOid ) values ('1', 'help', '1','1','1');
