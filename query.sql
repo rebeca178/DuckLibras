28b7682e0218b5481e0513f4197796ba3226d905
femboydev2025 <bsiqueira.dev@gmail.com>
2025-03-11 15:14:53 -0400

    Update query.sql
    Rebeca

--- query.sql
+++ query.sql
@@ -1,91 +1,51 @@
-
--- [RF02] Logar
-SELECT id, username, email FROM aluno 
-WHERE senha = MD5('123') AND email = 'email@email.com';
-
--- [RF03] Verificar o Cadastro
-SELECT id, username, email FROM aluno 
-WHERE username = 'Francisco' AND senha = MD5('123') AND email = 'email@email.com';
-
--- [RF04] Recuperação de dados
-SELECT id, username, email FROM aluno 
-WHERE email = 'email@email.com';
-
--- [RF05] Teste de Flashcard
-SELECT id_flashcard, quest, resposta FROM flashcards 
-WHERE quest = 'a';
-
--- [RF06] Alteração de login
-SELECT id, username, photo FROM aluno 
-WHERE username = 'Francisco' AND photo = 'photo';
-
--- [RF08] Limitação (nível no Board Square)
-SELECT id_bs, nivel, status FROM boardsquare 
-WHERE nivel = 1;
-
--- [RF09] Entrar no Board Square (casa de tabuleiro)
-SELECT id_bs, status, nivel FROM boardsquare 
-WHERE status = 1;
-
--- [RF10] Finalização do Board Square
-SELECT id_bs, status FROM boardsquare 
-WHERE status = 0;
-
--- [RF13] Ganho de nível no Board Square
+-- [RF12] Ganho de nível no Board Square
 SELECT id_bs, status, nivel FROM boardsquare 
 WHERE status = 0 AND nivel = 1;
 
--- [RF14] Flash Cards (pontuação)
+-- [RF13] FLASHCARD
+SELECT id_flashcard, pontos, nivel FROM flashcard;
+
+-- [RF13.1] Flash Cards (pontuação)
 SELECT id_flashcard, pontos, nivel FROM flashcards 
 WHERE pontos = 35;
 
--- [RF15] Emitir Aulas
+-- [RF14] Emitir Aulas
 SELECT id_aula, titulo, descricao, status FROM aulas;
 
--- [RF17] Resposta de Tradução
+-- [RF15] Buscar Tradução
 SELECT id_traducao, palavra, sinal, explicacao FROM traducao 
 WHERE id_traducao = 1;
 
--- [RF18] Listar Anotações
+-- [RF16] Listar Anotações
 SELECT id_anotacao, titulo, texto, imagem, data_criacao FROM anotacao;
 
--- [RF19] Loja (produtos e preços)
+-- [RF17] loja
+SELECT id_produto, produto, preco FROM loja;
+
+-- [RF18] Loja (produtos e preços)
 SELECT id_produto, produto, preco FROM loja 
 WHERE produto = 'a' AND preco = 1.00;
 
--- [RF20] Grupos de Alunos
+-- [RF19] Grupos de Alunos
 SELECT id, username, grupo_id FROM aluno 
-WHERE id = 1;
+WHERE id IN (1, 2, 3, 4, 5);
 
--- [RF21] Amizades no Chatbot
-SELECT id_chatbot, amizades FROM chatbot 
-WHERE amizades = 1;
+--SEGURANÇA
 
+--[RF10] LOGIN E SENHA
+SELECT a.photo ,a.username, a.email, a.senha FROM aluno;
 
 --RF[01]cadastrar
 SELECT * FROM aluno a WHERE a.username = 'Francisco' and a.senha = MD5('123') and a.email='email@email.com' and photo= 'photo';
 
-
---RF[02]Alteração de login
-SELECT * FROM aluno a WHERE a.username = 'Francisco' and photo= 'photo';
-
 --RF[03]Recuperação de dados
 SELECT * FROM aluno a WHERE a.email='email@email.com';
-
-
---RF[05]Entrar no Board square(casa de tabuleiro)
-SELECT * FROM BS b WHERE b.status ='1';
-
 
 --RF[06] Finalização do board square
 SELECT * FROM BS b WHERE b.status ='0';
 
 --RF[07] emissão de pontuação
 SELECT * FROM FLASHCARDS f WHERE f.pontos ='35';
-
---[RF08] “enviar” aula e atividade
-SELECT * FROM FLASHCARDS f WHERE f.status ='0';
-SELECT * FROM Aulas au WHERE a.status='0';
 
 --[RF09] Ganho de nível
 SELECT * FROM FLASHCARDS f WHERE f.nivel ='99';
@@ -111,9 +71,7 @@ WHERE email = 'email@example.com' AND senha = 'senha';
 
 -- RF03: Recuperação de senha (simplesmente verificando e-mail e senha)
 SELECT * FROM aluno 
-WHERE email = 'email@example.com';
-
--- RF04: Redirecionamento para home (não há necessidade de query, pois é um redirecionamento no app)
+WHERE email = 'email@example.com' and senha = MD5('123');
 
 -- RF05: Verificação de acesso ao Board Square
 SELECT * FROM BS 
