fdbabdc74f516b71c7a2ac758d12254c798bfdb5
Robson Elias <elias.freitas1205@gmail.com>
2025-03-12 16:41:43 -0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,10 @@
-use DuckLibras;
+USE DuckLibras;
+
+-- [RF16] Loja
+SELECT l.produto, l.preco, c.quantidade, a.username AS Aluno FROM loja l JOIN compra c ON l.id = c.loja_id JOIN aluno a ON a.id = c.ALID ORDER BY l.produto;
+
+-- [RF17] Grupos
+SELECT a1.username AS Aluno_1, a2.username AS Aluno_2 FROM aluno a1 JOIN aluno a2 ON a1.id != a2.id ORDER BY a1.username, a2.username;
 
 -- [RF20] Metas
 SELECT a.username AS Aluno, COUNT(DISTINCT fa.id_flashcard) AS Flashcards_Completados, COUNT(DISTINCT aa.id_aula) AS Aulas_Realizadas, p.pontos AS Pontuacao_Atual, p.nivel AS Nivel_Atual FROM aluno a LEFT JOIN flash_al fa ON fa.id_aluno = a.id LEFT JOIN au_al aa ON aa.id_aluno = a.id LEFT JOIN pontuacao p ON p.id = a.pontID GROUP BY a.id ORDER BY a.username;
