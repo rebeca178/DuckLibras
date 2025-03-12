0934b06a01143b1a5b8c79331f5330cf37382ebe
Guilhermepl9 <guidoarroxa2008@gmail.com>
2025-03-12 17:05:12 -0300

    query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,46 @@
 USE DuckLibras;
 
+--RF01 Cadastrar
+  SELECT *  from aluno a WHERE   a.username = 'francisco' and a.email='fran@gmail.com' and a.senha = MD5('1') and a.photo='franfoto';
+
+--[RF02] logar
+SELECT * FROM aluno a WHERE a.senha = MD5('1') and a.email='fran@gmail.com';
+
+--[RF03] Recuperação de dados
+SELECT * FROM aluno a WHERE a.username = 'francisco' and a.email='fran@gmail.com';
+
+--[RF04] Teste
+SELECT * FROM aluno a WHERE a.email='fran@gmail.com';
+
+--[RF05] Alteração de dados 
+SELECT * FROM aluno a WHERE a.username= 'joao' and a.photo= 'joaofoto' and a.email= 'joao@gmail.com';
+
+--[RF07] limitação
+SELECT * FROM pontuacao WHERE p.nivel = '1';
+
+--[RF08] Entrar no Board square(casa de tabuleiro)
+SELECT * FROM BS b;
+
+--[RF09] Finalização do board square
+SELECT * FROM BS b WHERE b.status = '0';
+
+--[RF11] Enviar conclusão
+SELECT * FROM BS b INNER JOIN aluno a ON b.auID = a.id WHERE  a.aluno ='joao' ;
+
+--[RF12] Ganho de nivel 
+SELECT * FROM pontuacao p WHERE p.pontos = '5' and p.nivel = '1';
+
+--[RF13] Flashcards
+SELECT * FROM pontcao p left JOIN Flashcards f  ON f.Pid = p.id WHERE p.nivel = '1' AND f.quest = 'WHAT?';
+
+--[RF14] Enviar Aula 
+SELECT * FROM pontucao p left join aulas a ON a.Pid = p.id WHERE p.nivel = '1' AND a.id= '1';
+
+--[RF15] anotações 
+SELECT * FROM traducao t left join Dicionario_Libras d on t.dicioId = d.id
+left join  aula a on a.dicioIdid = d.id
+WHERE t.id = '1' AND a.id = '1';
+
 -- [[RF16] Resposta Tradução
 SELECT t.palavra, t.created_at, t.updated_at, d.palavra AS Palavra_Libras FROM traducao t JOIN Dicionario_Libras d ON t.dicioId = d.id ORDER BY t.created_at DESC;
 
