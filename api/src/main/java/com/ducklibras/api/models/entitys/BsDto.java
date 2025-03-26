3f68023275197d53f8ab7fe2c0675ffcf301d1ce
pinguim <rebecafreitasreis7@gmail.com>
2025-03-26 17:22:40 -0300

    Update BsDto.java

--- api/src/main/java/com/ducklibras/api/models/entitys/BsDto.java
+++ api/src/main/java/com/ducklibras/api/models/entitys/BsDto.java
@@ -1,1 +1,36 @@
+package com.ducklibras.api.models.entitys;
 
+import java.io.Serializable;
+
+import com.ducklibras.api.models.entitys.BsDtos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "BS")
+public class BsDtos implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "status", nullable = false, columnDefinition = "boolean")
+    private String status;
+
+    
+
+
+    
+
+}
