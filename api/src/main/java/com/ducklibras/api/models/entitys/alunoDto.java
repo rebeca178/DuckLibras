e0e3e2627895c98d03ea13541ca6562ddacd907f
Luany <luanytarga@gmail.com>
2025-03-26 16:26:34 -0300

    feito com sucesso entitys

--- api/src/main/java/com/ducklibras/api/models/entitys/alunoDto.java
+++ api/src/main/java/com/ducklibras/api/models/entitys/alunoDto.java
@@ -2,6 +2,11 @@ package com.api.ducklibras.models.dtos;
 
 import com.api.ducklibras.models.entitys.UsersEntity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +20,11 @@ public class AlunoDto implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GneratedValue(strategy = jakart.persiste)
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id; 
     
-    @column(name = "names", nullable = false, length = 160)
-    private String names;
+    @column(name = "username", nullable = false, length = 255)
+    private String username;
      
     @column(name = "pass", nullable = false, length = "TEXT")
     private String pass;
@@ -27,8 +32,27 @@ public class AlunoDto implements Serializable{
     @column(name = "email", nullable = false, length = "160")
     private String email;
 
-    @column(name = "pass", nullable = false, length = "TEXT")
-    private String pass;
+    @column(name = "photo", nullable = false, length = "160")
+    private String photo;
+
+    @ManyToOne
+    @JoinColumn(name = "TradId", nullable = false)
+    private TraducaoEntity TradId;
+
+    @ManyToMany
+    @JoinTable(
+        name = "au_al",
+        joinColumns = @JoinColumn(name = "id_aula"),
+        inverseJoinColumns = @JoinColumn(name = "id_aluno")
+    )
+    
+    @OneToMany
+    @JoinColumn(name = "BSid", nullable = false)
+    private BSEntity BSid;
+
+    @OneToOne
+    @JoinColumn(name = "pontID", nullable = false)
+    private PontacaoEntity TradId;
 
     public UserEntity()(
     
