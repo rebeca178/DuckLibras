4c3a610bface1492029a4d15bb73519c4bee4e13
Luany <luanytarga@gmail.com>
2025-03-26 15:02:35 -0300

    feito com sucesso entitys

--- api/src/main/java/com/ducklibras/api/models/entitys/alunoDto.java
+++ api/src/main/java/com/ducklibras/api/models/entitys/alunoDto.java
@@ -1,3 +1,37 @@
-public class alunoDto {
+package com.api.springapi.models.dtos;
+
+import com.api.springapi.models.entitys.UsersEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "aluno")
+public class AlunoDto implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GneratedValue(strategy = jakart.persiste)
+    private Long id; 
+    
+    @column(name = "names", nullable = false, length = 160)
+    private String names;
+     
+    @column(name = "pass", nullable = false, length = "TEXT")
+    private String pass;
+    
+    @column(name = "emnail", nullable = false, length = "160")
+    private float email;
+
+    @column(name = "pass", nullable = false, length = "TEXT")
+    private String pass;
+
+    public UserEntity()(
+    
+    )
 
 }
