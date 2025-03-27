76d516cd1d2bcd3cab8ee6672c779ae7258ace3f
Robson Elias <elias.freitas1205@gmail.com>
2025-03-26 20:04:28 -0400

    Create AnotacaoDTO.java
    dto de "anotacao"

---
+++ api/src/main/java/com/ducklibras/api/models/dtos/AnotacaoDTO.java
@@ -0,0 +1,15 @@
+package com.ducklibras.api.models.dtos;
+
+public class AnotacaoDTO {
+
+    private String conteudo;
+
+    // Getters e Setters
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
