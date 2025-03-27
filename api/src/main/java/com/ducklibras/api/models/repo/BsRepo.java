313835d2da229d41188fbf0be9774f6587e1d40d
roodguees <rebecafreitasreis7@gmail.com>
2025-03-27 15:59:33 -0300

    a

--- api/src/main/java/com/ducklibras/api/models/repo/BsRepo.java
+++ api/src/main/java/com/ducklibras/api/models/repo/BsRepo.java
@@ -1,1 +1,11 @@
 package com.ducklibras.api.models.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ducklibras.api.models.entitys.BsEntitys;
+
+@Repository
+public interface BsRepo extends JpaRepository<BsEntitys, Long> {
+
+}
