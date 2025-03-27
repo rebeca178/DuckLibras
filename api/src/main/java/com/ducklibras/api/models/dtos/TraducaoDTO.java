0085ee5b4eab4f42d08f39678ffe755b9ce6878b
Robson Elias <elias.freitas1205@gmail.com>
2025-03-26 20:04:04 -0400

    Create TraducaoDTO.java
    dto de "traducao"

---
+++ api/src/main/java/com/ducklibras/api/models/dtos/TraducaoDTO.java
@@ -0,0 +1,24 @@
+package com.ducklibras.api.models.dtos;
+
+public class TraducaoDTO {
+
+    private String conteudo;
+    private String idioma;
+
+    // Getters e Setters
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+}
