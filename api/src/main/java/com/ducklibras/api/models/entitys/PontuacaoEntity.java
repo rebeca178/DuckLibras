e9851b9afd743c1ae4e9e61365023b9f1400ca8d
roodguees <rebecafreitasreis7@gmail.com>
2025-03-28 14:59:57 -0300

    a

--- api/src/main/java/com/ducklibras/api/models/entitys/PontuacaoEntity.java
+++ api/src/main/java/com/ducklibras/api/models/entitys/PontuacaoEntity.java
@@ -1,5 +1,16 @@
 package com.ducklibras.api.models.entitys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class PontuacaoEntity {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
 
 }
