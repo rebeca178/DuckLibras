ecb0cdf478020c479f90ce81a9fcef322b55b9c8
Robson Elias <elias.freitas1205@gmail.com>
2025-03-27 21:55:04 -0400

    Update TraducaoDTO.java

--- api/src/main/java/com/ducklibras/api/models/dtos/TraducaoDTO.java
+++ api/src/main/java/com/ducklibras/api/models/dtos/TraducaoDTO.java
@@ -1,24 +1,16 @@
-package com.ducklibras.api.models.dtos;
+package com.seuprojeto.models.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TraducaoDTO {
-
-    private String conteudo;
-    private String idioma;
-
-    // Getters e Setters
-    public String getConteudo() {
-        return conteudo;
-    }
-
-    public void setConteudo(String conteudo) {
-        this.conteudo = conteudo;
-    }
-
-    public String getIdioma() {
-        return idioma;
-    }
-
-    public void setIdioma(String idioma) {
-        this.idioma = idioma;
-    }
+    private Long id;
+    private String idiomaOrigem;
+    private String idiomaDestino;
+    private String textoOriginal;
+    private String textoTraduzido;
 }
