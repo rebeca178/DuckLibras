5677c9255cca57e9e1487384675df3cf2128b863
roodguees <rebecafreitasreis7@gmail.com>
2025-03-28 14:54:44 -0300

    a

---
+++ api/src/main/java/com/ducklibras/api/models/dtos/BsDto.java
@@ -0,0 +1,20 @@
+package com.ducklibras.api.models.dtos;
+
+import com.ducklibras.api.models.entitys.AulasEntity;
+import com.ducklibras.api.models.entitys.FlashcardEntity;
+import com.ducklibras.api.models.entitys.PontuacaoEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class BsDto{
+
+    private long id;
+    private boolean status;
+    private AulasEntity Aulas_id;
+    private FlashcardEntity flashId;
+    private PontuacaoEntity ponID;
+}
