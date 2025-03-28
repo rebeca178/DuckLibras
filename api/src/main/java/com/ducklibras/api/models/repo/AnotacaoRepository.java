247627351a394ddb27dbd439d3e73aebb43aac14
roodguees <rebecafreitasreis7@gmail.com>
2025-03-28 16:25:01 -0300

    a

--- api/src/main/java/com/ducklibras/api/models/repo/AnotacaoRepository.java
+++ api/src/main/java/com/ducklibras/api/models/repo/AnotacaoRepository.java
@@ -1,4 +1,4 @@
-package com.ducklibras.api.models.repositorios;
+package com.ducklibras.api.models.repo;
 
 import com.ducklibras.api.models.entitys.AnotacaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
