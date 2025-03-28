131a47cfbdd981eac2d3d7191910b6bff7bcf919
Robson Elias <elias.freitas1205@gmail.com>
2025-03-27 22:35:38 -0400

    Update AnotacaoRepository.java

--- api/src/main/java/com/ducklibras/api/models/repo/AnotacaoRepository.java
+++ api/src/main/java/com/ducklibras/api/models/repo/AnotacaoRepository.java
@@ -5,25 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AnotacaoRepository extends JpaRepository<AnotacaoEntity, Long> {
 
-    // Método para encontrar todas as anotações por título
-    List<AnotacaoEntity> findByTitulo(String titulo);
+    // Método para encontrar todas as anotações por Dicionário
+    List<AnotacaoEntity> findByDicioId(Long dicioId);
 
-    // Método para encontrar anotações por conteúdo (utilizando 'like' para buscas parciais)
-    List<AnotacaoEntity> findByConteudoContaining(String conteudo);
+    // Método para buscar uma anotação por ID
+    Optional<AnotacaoEntity> findById(Long id);
 
-    // Método para encontrar anotações que possuem um título específico e ordená-las por data (descendente)
-    List<AnotacaoEntity> findByTituloOrderByDataCriacaoDesc(String titulo);
+    // Método para buscar todas as anotações associadas a um BS
+    List<AnotacaoEntity> findByBsId(Long bsId);
 
-    // Método para encontrar anotações por conteúdo e ordenar por título (ascendente)
-    List<AnotacaoEntity> findByConteudoContainingOrderByTituloAsc(String conteudo);
-    
-    // Método para contar o número de anotações com um determinado título
-    long countByTitulo(String titulo);
-
-    // Método para excluir anotações por título
-    void deleteByTitulo(String titulo);
+    // Método para excluir anotações por Dicionário
+    void deleteByDicioId(Long dicioId);
 }
