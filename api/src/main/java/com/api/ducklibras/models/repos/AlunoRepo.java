dc4223c77a67e729f39b2019f55541b7448f059a
Luany <dhemerson.juniorr6000@gmail.com>
2025-03-28 15:32:13 -0300

    ta

--- api/src/main/java/com/api/ducklibras/models/repos/AlunoRepo.java
+++ api/src/main/java/com/api/ducklibras/models/repos/AlunoRepo.java
@@ -1,15 +1,12 @@
 package com.api.ducklibras.models.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import com.api.ducklibras.models.Entity.AlunoEntity;
 
 @Repository
 public interface AlunoRepo extends JpaRepository<AlunoEntity, Long> {
-
-    @Query("SELECT u FROM AlunoEntity u WHERE u.username = ?1 AND u.email = ?2")
-    AlunoEntity findByUsernameAndEmail(String usename, String email);
 
 }
