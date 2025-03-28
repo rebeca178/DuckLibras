738f1bf3749562f34db8f1164bd4313ccd672ce4
roodguees <rebecafreitasreis7@gmail.com>
2025-03-28 16:12:02 -0300

    a

--- api/src/main/java/com/ducklibras/api/models/repo/TraducaoRepository.java
+++ api/src/main/java/com/ducklibras/api/models/repo/TraducaoRepository.java
@@ -1,4 +1,4 @@
-package com.ducklibras.api.models.repositorios;
+package com.ducklibras.api.models.repo;
 
 import com.ducklibras.api.models.entitys.TraducaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
