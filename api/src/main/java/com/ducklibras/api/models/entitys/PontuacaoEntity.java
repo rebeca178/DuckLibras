e398cecbfc01680cb63c5cbcea978aca4bf887fe
roodguees <rebecafreitasreis7@gmail.com>
2025-03-28 15:48:10 -0300

    a

--- api/src/main/java/com/ducklibras/api/models/entitys/PontuacaoEntity.java
+++ api/src/main/java/com/ducklibras/api/models/entitys/PontuacaoEntity.java
@@ -1,16 +1,35 @@
 package com.ducklibras.api.models.entitys;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 @Entity
-@Table
-public class PontuacaoEntity {
+@Table(name = "Pontuacao")
+public class PontuacaoEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private Long ponId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "pontos", nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int pontos;
+
+    @Column(name = "nivel", nullable = false, columnDefinition = "INT DEFAULT 1")
+    private int nivel;
+
+    @Column(name = "ultima_atualizacao", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private String ultimaAtualizacao;
 }
