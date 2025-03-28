971af17123c679bc99b9853ce7847d4b4d6eda32
Robson Elias <elias.freitas1205@gmail.com>
2025-03-27 22:36:10 -0400

    Update TraducaoRepository.java

--- api/src/main/java/com/ducklibras/api/models/repo/TraducaoRepository.java
+++ api/src/main/java/com/ducklibras/api/models/repo/TraducaoRepository.java
@@ -5,22 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TraducaoRepository extends JpaRepository<TraducaoEntity, Long> {
 
-    // Método para encontrar todas as traduções por idioma de origem
-    List<TraducaoEntity> findByIdiomaOrigem(String idiomaOrigem);
+    // Método para encontrar traduções por Dicionário
+    List<TraducaoEntity> findByDicioId(Long dicioId);
 
-    // Método para encontrar traduções por texto original (utilizando 'like' para buscas parciais)
-    List<TraducaoEntity> findByTextoOriginalContaining(String textoOriginal);
+    // Método para buscar uma tradução por ID
+    Optional<TraducaoEntity> findById(Long id);
 
-    // Método para encontrar traduções por idioma de origem e idioma de destino
-    List<TraducaoEntity> findByIdiomaOrigemAndIdiomaDestino(String idiomaOrigem, String idiomaDestino);
-
-    // Método para contar o número de traduções com um determinado idioma de origem
-    long countByIdiomaOrigem(String idiomaOrigem);
-
-    // Método para excluir traduções por idioma de origem
-    void deleteByIdiomaOrigem(String idiomaOrigem);
+    // Método para excluir traduções por Dicionário
+    void deleteByDicioId(Long dicioId);
 }
