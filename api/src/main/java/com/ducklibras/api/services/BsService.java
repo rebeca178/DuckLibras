97b83fa1997ff68c62b5476f18877e3e0f47305e
roodguees <rebecafreitasreis7@gmail.com>
2025-04-03 15:25:26 -0300

    api update

--- api/src/main/java/com/ducklibras/api/services/BsService.java
+++ api/src/main/java/com/ducklibras/api/services/BsService.java
@@ -2,20 +2,6 @@ package com.ducklibras.api.services;
 
 import org.springframework.stereotype.Service;
 
-
-import com.ducklibras.api.models.repo.BsRepo;
-
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-
 @Service
 public class BsService {
-
-    @Autowired
-    private BsRepo bsRepo;
-
-
-    
 }
