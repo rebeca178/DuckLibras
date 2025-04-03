dbf99935a057d5f83be8f3d7af3a93ccb94a0bcf
roodguees <rebecafreitasreis7@gmail.com>
2025-04-03 14:49:01 -0300

    api update

--- api/src/main/java/com/ducklibras/api/services/BsService.java
+++ api/src/main/java/com/ducklibras/api/services/BsService.java
@@ -2,14 +2,10 @@ package com.ducklibras.api.services;
 
 import org.springframework.stereotype.Service;
 
-import com.ducklibras.api.models.dtos.BsDto;
-import com.ducklibras.api.models.dtos.PontuacaoDto;
-import com.ducklibras.api.models.entitys.BsEntitys;
-import com.ducklibras.api.models.entitys.PontuacaoEntity;
+
 import com.ducklibras.api.models.repo.BsRepo;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,14 +16,6 @@ public class BsService {
     @Autowired
     private BsRepo bsRepo;
 
-    // abri bs
-    public Optional<BsDto> abrirBs (Long alunoId, int nivel,boolean status) {
-        BsEntitys nivelBs = bsRepo.findById(alunoId).get();
-        if(nivelBs == null) return Optional.empty();
-
-
-
-    }
 
     
 }
