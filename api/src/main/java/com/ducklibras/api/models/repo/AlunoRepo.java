157332bc1994c9c7796f29cc27a0456a64343c26
Desktop <dhemerson.juniorr6000@gmail.com>
2025-04-04 00:13:55 -0300

    alteração no  AlunoRepo feito com sucesso

--- api/src/main/java/com/ducklibras/api/models/repo/AlunoRepo.java
+++ api/src/main/java/com/ducklibras/api/models/repo/AlunoRepo.java
@@ -1,12 +1,18 @@
 package com.ducklibras.api.models.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ducklibras.api.models.entitys.AlunoEntitys;
 
 @Repository
 public interface AlunoRepo extends JpaRepository<AlunoEntitys, Long> {
+    
+        @Query("SELECT u FROM UsersEntity u WHERE u.username = ?1 AND u.email = ?2")
+    public Optional<AlunoEntitys> findByUsernameAndEmail(String username, String email);
+
 
 }
