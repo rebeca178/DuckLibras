edfcdffbed86e4928fbd45633d944c6528fb5ed2
Robson Elias <elias.freitas1205@gmail.com>
2025-04-04 14:22:16 -0400

    Create TraducaoService.java
    Service de "traducao"

---
+++ api/src/main/java/com/ducklibras/api/services/TraducaoService.java
@@ -0,0 +1,45 @@
+package com.ducklibras.api.services;
+
+import com.ducklibras.api.models.entitys.TraducaoEntity;
+import com.ducklibras.api.repositories.TraducaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TraducaoService {
+    
+    @Autowired
+    private TraducaoRepository traducaoRepository;
+
+    public TraducaoEntity salvarTraducao(TraducaoEntity traducao) {
+        return traducaoRepository.save(traducao);
+    }
+
+    public List<TraducaoEntity> listarTodas() {
+        return traducaoRepository.findAll();
+    }
+
+    public Optional<TraducaoEntity> buscarPorId(Long id) {
+        return traducaoRepository.findById(id);
+    }
+
+    public List<TraducaoEntity> buscarPorIdioma(String idioma) {
+        return traducaoRepository.findByIdioma(idioma);
+    }
+
+    public TraducaoEntity atualizarTraducao(Long id, TraducaoEntity traducaoAtualizada) {
+        return traducaoRepository.findById(id).map(traducao -> {
+            traducao.setTextoOriginal(traducaoAtualizada.getTextoOriginal());
+            traducao.setTextoTraduzido(traducaoAtualizada.getTextoTraduzido());
+            traducao.setIdiomaOrigem(traducaoAtualizada.getIdiomaOrigem());
+            traducao.setIdiomaDestino(traducaoAtualizada.getIdiomaDestino());
+            return traducaoRepository.save(traducao);
+        }).orElseThrow(() -> new RuntimeException("Tradução não encontrada."));
+    }
+
+    public void deletarTraducao(Long id) {
+        traducaoRepository.deleteById(id);
+    }
+}
