950aa41fc27d0e5ca7cc89ffdb8c0e80e760ef90
Robson Elias <elias.freitas1205@gmail.com>
2025-04-09 17:35:24 -0300

    Update PontuacaoService.java

--- api/src/main/java/com/ducklibras/api/services/PontuacaoService.java
+++ api/src/main/java/com/ducklibras/api/services/PontuacaoService.java
@@ -3,7 +3,6 @@ package com.ducklibras.api.services;
 import com.ducklibras.api.models.dtos.PontuacaoDto;
 import com.ducklibras.api.models.entitys.PontuacaoEntity;
 import com.ducklibras.api.models.repo.PontuacaoRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,7 +11,6 @@ import java.util.Optional;
 @Service
 public class PontuacaoService {
 
-    @Autowired
     private PontuacaoRepo pontuacaoRepo;
 
     /**
@@ -23,8 +21,9 @@ public class PontuacaoService {
      * @return A entidade de pontuação atualizada.
      */
     public Optional<PontuacaoDto> adicionarPontos(Long userId, int pontos) {
-        PontuacaoEntity pontuacao = pontuacaoRepo.findById(userId).get();
-        if(pontuacao == null) return Optional.empty();
+        Optional<PontuacaoEntity> optionalPontuacao = pontuacaoRepo.findById(userId);
+        if (!optionalPontuacao.isPresent()) return Optional.empty();
+        PontuacaoEntity pontuacao = optionalPontuacao.get();
 
         // Adiciona os pontos
         pontuacao.setPontos(pontuacao.getPontos() + pontos);
