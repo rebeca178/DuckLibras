56c051ace1922de316a920dee5395fa47331aa83
Robson Elias <elias.freitas1205@gmail.com>
2025-04-16 16:17:02 -0300

    Update population.sql
    o bruninho eh fofo :3

--- population.sql
+++ population.sql
@@ -1,18 +1,14 @@
-
 use ducklibras;
 
-INSERT INTO chatbot (id_chat,chat, perguntas, respostas,amizade) VALUES ('1', 'Quem sou eu ?', 'Você é um humano ','1');
+insert into chatbot (id_chat,chat, perguntas, respostas,amizade) VALUES ('1', 'Quem sou eu ?', 'Você é um humano ','1');
 
-INSERT INTO loja (id_lol, produto, preco) VALUES ('1', 'pato aranha', '100P');
+insert into loja (id_lol, produto, preco) VALUES ('1', 'pato aranha', '100P');
 
-INSERT INTO compra (id_cop, quantidade, loja_id,ALID) VALUES ('1', '1','1','1');
-
-USE DuckLibras;
+insert into compra (id_cop, quantidade, loja_id,ALID) VALUES ('1', '1','1','1');
 
 insert into dicionario_libras (id,palavra,traducao,sinal,imagem,video_url,descricao,categoria,dificuldade) VALUES  ('1','coelho','coelho2','sinalcoelho', 'imgcoleho','vidocoelho','oqcoelho','animal','Fácil');
 
 insert into pontuacao (id,pontos,nivel) VALUES  ('1','5','1');
-
 
 insert into BS (id_bs,STATUS,id_aula,id_flashcard,nivel) VALUES  ('1','1','1','1','1');
 
@@ -29,6 +25,7 @@ insert into anotacao (id,titulo,texto,dicioId,BSid) VALUES  ('1','aula1','sinais
 insert into loja (id,produto,preco) VALUES  ('1','patobatman','2.50');
 
 insert into aluno (id,username,email,photo,senha,Anoid,TradId,BSid,pontID) VALUES  ('1','francisco','fran@gmail.com','franfoto',MD5 ('1'),'1','1','1','1');
+
 insert into aluno (id,username,email,photo,senha,Anoid,TradId,BSid,pontID) VALUES  ('2','joao','joao@gmail.com','joaofoto',MD5 ('2'),'1','1','1','1');
 
 insert into chat(id,mensagem,remetente,destinatario) VALUES  ('1','oi','1','2');
@@ -40,4 +37,3 @@ insert into au_al(id_aula,id_aluno) VALUES  ('1','1');
 insert into compra (id,quantidade,loja_id,ALID ) values ('1', '1', '1','1');
 
 insert into dicas (ID,ajuda,Fid,Cid,DICIOid ) values ('1', 'help', '1','1','1');
-
