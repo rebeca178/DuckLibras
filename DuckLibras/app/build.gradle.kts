76247e3b971ba93bf25104b2a045256d3076b5bd
Victor <vitinho.vgzin@gmail.com>
2025-05-23 15:07:28 -0300

    cachaça

--- DuckLibras/app/build.gradle.kts
+++ DuckLibras/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ducklibras"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ducklibras"
         minSdk = 24
-        targetSdk = 34
+        this.targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
