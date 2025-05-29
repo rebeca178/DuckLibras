c4114cc8aeac48b3d77bf9b5df910eb683943ad0
Desktop <dhemerson.juniorr6000@gmail.com>
2025-05-29 11:07:44 -0300

    a

--- MobileDuckLibras/app/src/main/java/com/example/mobileducklibras/Dicionario/presetation/DicionarioActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.ducklibras.Dicionario.presetation;
-
-public class DicionarioActivity {
-}
--- MobileDuckLibras/app/src/main/java/com/example/mobileducklibras/FlashCard/presetation/FlashCardActivity.java
+++
@@ -1,2 +0,0 @@
-package com.example.ducklibras.FlashCard.presetation;public class FlashCardActivity {
-}
