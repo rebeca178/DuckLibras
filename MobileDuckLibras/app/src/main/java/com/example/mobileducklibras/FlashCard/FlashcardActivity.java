24519cb9d0c2298a01165a7af91f623c5040e230
ManubsProgramming <manuela.j.gamer1968@gmail.com>
2025-06-02 14:46:33 -0300

    update mob

---
+++ MobileDuckLibras/app/src/main/java/com/example/mobileducklibras/FlashCard/FlashcardActivity.java
@@ -0,0 +1,4 @@
+package com.example.mobileducklibras.FlashCard;
+
+public class FlashcardActivity {
+}
---
+++ MobileDuckLibras/app/src/main/java/com/example/mobileducklibras/Home/Home.java
@@ -0,0 +1,16 @@
+package com.example.mobileducklibras.Home;
+
+import android.content.Intent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobileducklibras.FlashCard.FlashcardActivity;
+
+public class Home extends AppCompatActivity {
+
+    public void flashcard_btn(View view) {
+        Intent intent = new Intent(this, FlashcardActivity.class);
+        startActivity(intent);
+    }
+}
