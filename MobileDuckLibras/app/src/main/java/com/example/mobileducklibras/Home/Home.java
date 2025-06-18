b48185ace056564d4494822c97912382079528dc
Rebeca Freitas Reis <rebecafreitasreis7@gmail.com>
2025-06-18 15:33:44 -0300

    Update Home.java

--- MobileDuckLibras/app/src/main/java/com/example/mobileducklibras/Home/Home.java
+++ MobileDuckLibras/app/src/main/java/com/example/mobileducklibras/Home/Home.java
@@ -5,7 +5,12 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mobileducklibras.Aulas.Aula1Activity;
 import com.example.mobileducklibras.FlashCard.FlashcardActivity;
+import com.example.mobileducklibras.Traducao.TraducaoActivity;
+import com.example.mobileducklibras.anotacao.AnotacaoActivity;
+import com.example.ducklibras.Dicionario.presetation.DicionarioActivity;
+import com.example.ducklibras.user.presetation.ProfileActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -13,6 +18,30 @@ public class Home extends AppCompatActivity {
         Intent intent = new Intent(this, FlashcardActivity.class);
         startActivity(intent);
     }
+    public void anotacao_btn(View view) {
+      Intent intent = new Intent(this, AnotacaoActivity.class);
+     startActivity(intent);
+   }
 
-    //teste
+    public void dicionario_btn(View view) {
+     Intent intent = new Intent(this, DicionarioActivity.class);
+     startActivity(intent);
+    }
+
+     public void traducao_btn(View view) {
+    Intent intent = new Intent(this, TraducaoActivity.class);
+    startActivity(intent);
+    }
+
+    public void user_btn(View view) {
+    Intent intent = new Intent(this, ProfileActivity.class);
+    startActivity(intent);
+    }
+
+    public void bsativo_btn(View view) {
+         Intent intent = new Intent(this, Aula1Activity.class);
+        startActivity(intent);
+        }
+
+
 }
